a75b385501c658ff2d696fcb1f605d9846a3d7b9
Bola Akinmarin <0fd1c8c8@students.codeinstitute.net>
2024-07-25 15:59:10 +0000

    feat: Add functions to display welcome message and get user details

--- run.py
+++ run.py
@@ -14,8 +14,28 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('the-cleaning-hack-calculator')
 
-quotes = SHEET.worksheet('quotes')
-data = quotes.get_all_values()
+#quotes = SHEET.worksheet('quotes')
+#data = quotes.get_all_values()
+#print(data)
 
-print(data)
+def hello_user():
+    """
+    Display welcome message and instructions to user.
+    """
+    print('Welcome to The Cleaning Hack!')
+    print('To get your free cleaning estimate, you will need to provide your contact details and answer a few questions regarding your property.')
+    print('Please confirm if you wish to continue...\n')
 
+    choice_str = input('Do you wish to continue? Y or N: ')
+    
+hello_user()
+
+def get_user_details():
+    """
+    Get details personal details from user and add to sheet.
+    """
+    name_str = input('\nEnter your name: ')
+    mobile_str = input('Enter your mobile number: ')
+    email_str = input('Enter your email address: ')
+
+get_user_details()   
